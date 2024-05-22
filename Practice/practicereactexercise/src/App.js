b7687a6b71df8ba03c2f84941cffb65c6c97651e
Parker Wallace <wallace.parker@hotmail.com>
2024-05-22 09:24:46 -0230

    added "classwork app for future routing"

--- Practice/practicereactexercise/src/App.js
+++ Practice/practicereactexercise/src/App.js
@@ -9,18 +9,12 @@ function App() {
   // const [nameofstate, function that sets state] = useState(initialstate);
   const [weather, setWeatherLocation] = useState(localweatherstjohns);
   useEffect(() => {
-    console.log(weather);
-  }, [weather]);
+    setInterval(() => {
+      setWeatherLocation(weather == localweatherstjohns ? localweatherottowa : localweatherstjohns)
+    }, 5000);
+  }, []);
   return (
     <div className="App">
-      <button onClick={()=>{
-        if (weather.city == "St Jonn's") {
-          setWeatherLocation(localweatherottowa)
-        }
-        else {
-          setWeatherLocation(localweatherstjohns)
-        }
-      }}>Click Here</button>
       <h2>Parker's Weather</h2>
       <WeatherList localweather={weather}></WeatherList>
     </div>
